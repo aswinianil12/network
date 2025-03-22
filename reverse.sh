a57226a9aedb38a2a081f2c8c9e44eeb37619b62
aswinianil12 <aswinianil918@gmail.com>
2025-03-22 09:40:10 +0530

    Create reverse.sh

---
+++ reverse.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Enter a number:"
+read num
+reverse=$(echo "$num"|rev)
+if ["$num" -eq "$reverse"];
+then
+echo "$num is same when reversed"
+else
+echo "$num is not same when reversed"
+fi
